0c5a861968bd0439d5c227c899c02669a396ce8b
Alvin Yao <ayao00@stuy.edu>
2019-12-05 10:26:46 -0500

    a

--- main.c
+++ main.c
@@ -8,6 +8,9 @@
 #include <time.h>
 #include <sys/types.h>
 
+static void sighandler(int signo){
+  
+}
 char ** parse_args( char * line ){
   char ** parsed_args = malloc(256);
   char * current;
@@ -26,22 +29,24 @@ int main(int argc, char * argv[]){
   char * line = malloc(256);
   char * parsed = malloc(256);
   char * current = malloc(256);
-  if(argc <= 1){
-    printf("Enter the command line invocation: ");
-    fgets(s, 256, stdin);
-    s[strlen(s)-1]=0;
-  }else{
-    s = argv[1];
-  }
   while(1){
     printf("%s\n",getcwd(current, 256));
-    return 0;
+    if(argc <= 1){
+      printf("Enter the command line invocation: ");
+      fgets(s, 256, stdin);
+      s[strlen(s)-1]=0;
+    }else{
+      s = argv[1];
+    }
 
+    return 0;
 
   }
   int f, child,status;
   f = fork();
   if(f){
+    char ** args = parse_args(s);
+    execvp(args[0] , args);
     child = wait(&status);
     printf("parent. pid: %d f: %d parent :%d\n", getpid(), f, getppid());
   }else{
